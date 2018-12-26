1e96124ca01b79f4a1a35466a9731505214a7d30
Olexandr Matkovskyi <savio.km.ua@gmail.com>
2018-12-26 14:06:31 +0200

    [12] Code formatting

--- app/controllers/tariffs_controller.rb
+++ app/controllers/tariffs_controller.rb
@@ -1,42 +1,44 @@
+# frozen_string_literal: true
+
 class TariffsController < ApplicationController
+  before_action :bundle_tariff, only: %i[show edit update destroy]
+
   def index
     @tariff = Tariff.all
   end
+
   def new
     @tariff = Tariff.new
   end
-  def show
-    bundle_tariff
-  end
-  def edit
-    bundle_tariff
-  end
+
   def update
-    bundle_tariff
     if @tariff.update(tariff_params)
-      redirect_to @tariff
+      redirect_to tariffs_path
     else
       render 'edit'
     end
   end
+
   def destroy
-    bundle_tariff
     @tariff.destroy
     redirect_to tariffs_path
   end
+
   def create
-    # render plaine: params[:post].inspect
     @tariff = Tariff.new(tariff_params)
     if @tariff.save
       redirect_to tariffs_path
     else
-      render 'new'
+      render :new
     end
   end
+
   private
+
   def bundle_tariff
     @tariff = Tariff.find(params[:id])
   end
+
   def tariff_params
     params.require(:tariff).permit(:name, :price)
   end
--- db/migrate/20181219095843_create_tariffs.rb
+++ db/migrate/20181219095843_create_tariffs.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateTariffs < ActiveRecord::Migration[5.2]
   def change
     create_table :tariffs do |t|
